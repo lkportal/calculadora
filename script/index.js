5506db912e59e9af46b46e6a5a74ab346c7677c4
lkportal <lksxim@gmail.com>
2024-01-24 20:27:06 -0300

    modificando estrutura

--- script/index.js
+++ script/index.js
@@ -1,76 +1,85 @@
 "use strict";
 const btn = document.querySelectorAll('button');
 const visor = document.getElementById('visor');
-const ptext = document.getElementById('p');
 const results = document.getElementById('n=');
 const soma = document.getElementById('n+');
 const sub = document.getElementById('n-');
 const multiplicar = document.getElementById('nx');
 const dividir = document.getElementById('n/');
+const clear = document.getElementById('C');
 let btns = '';
+let armazenandoAcumulador = 0;
 let result;
-let operado;
 let numeros;
 let numero2;
-const regex = /[^\W]/gi;
+let char = '';
 btn.forEach((element) => {
-    element.addEventListener('click', (ev) => {
-        const { target } = ev;
-        if (target) {
-            const dado1 = target.textContent;
-            if (dado1) {
-                btns += dado1;
-                numeros = parseFloat(btns);
+    try {
+        element.addEventListener('click', (ev) => {
+            const { target } = ev;
+            if (target) {
+                const dado1 = target.textContent;
+                if (dado1) {
+                    btns += dado1;
+                    numero2 = parseFloat(btns);
+                    visor.value = btns.toString();
+                    //O sinal recebido pelo btns altera o valor do resultado das expressões 
+                }
             }
-        }
-        visor.value = btns.toString();
-    });
+        });
+    }
+    catch (error) {
+        throw new Error("Erro de conversão");
+    }
+});
+clear.addEventListener("click", () => {
+    if (btns.includes('c') || btns.includes('C')) {
+        btns = '';
+        numeros = 0;
+        numero2 = 0;
+        result = 0;
+        visor.value = '0';
+    }
 });
 soma.addEventListener('click', () => {
-    if (result) {
-        numeros = result;
+    if (btns.includes("+")) {
+        numeros = parseFloat(btns);
+        console.log(numeros, 'vinda função +');
+        console.log(btns, 'btns');
+        btns = '';
+        char = '+';
     }
-    numero2 = numeros;
-    operado = btns;
-    btns = '';
+    else {
+        visor.value = '';
+    }
 });
 sub.addEventListener('click', () => {
-    if (result) {
-        numeros = result;
+    if (btns.includes("-")) {
+        numeros = parseFloat(btns);
+        console.log(numeros, 'vinda função -');
+        console.log(btns, 'btns');
+        btns = '';
+        char = '-';
     }
-    numero2 = numeros;
-    operado = btns;
-    btns = '';
-});
-multiplicar.addEventListener('click', () => {
-    if (result) {
-        numeros = result;
-    }
-    numero2 = numeros;
-    operado = btns;
-    btns = '';
-});
-dividir.addEventListener('click', () => {
-    if (result) {
-        numeros = result;
-    }
-    numero2 = numeros;
-    operado = btns;
-    btns = '';
 });
 results === null || results === void 0 ? void 0 : results.addEventListener('click', () => {
-    if (operado.includes('+')) {
-        result = numeros + numero2;
+    if (btns.includes("=")) {
+        visor.value = '';
+        if (char.includes('+') && btns.includes('')) {
+            result = numero2 + numeros;
+            armazenandoAcumulador = result;
+            // if(char.includes('+') || btns != '') {
+            //      console.log(armazenandoAcumulador)
+            //     result = armazenandoAcumulador + numero2
+            // } 
+        }
+        else if (char.includes('-') && btns.includes('')) {
+            result = numeros - numero2;
+        }
     }
-    else if (operado.includes('-')) {
-        numeros > numero2 ? result = numeros - numero2 : result = numero2 - numeros;
-    }
-    else if (operado.includes('x')) {
-        result = numero2 * numeros;
-    }
-    else if (operado.includes('/')) {
-        result = numero2 / numeros;
-    }
+    console.log(numeros, 'numero1');
+    console.log(numero2, 'numero2');
+    console.log(result, 'resultado');
     btns = '';
     visor.value = result.toString();
 });
--- script/index.ts
+++ script/index.ts
@@ -1,87 +1,119 @@
 
 const btn = document.querySelectorAll('button') 
 const visor = document.getElementById('visor') as HTMLInputElement
-const ptext = document.getElementById('p')
 const results = document.getElementById('n=') 
 const soma = document.getElementById('n+') as HTMLButtonElement
 const sub = document.getElementById('n-') as HTMLButtonElement
 const multiplicar = document.getElementById('nx') as HTMLButtonElement
 const dividir = document.getElementById('n/') as HTMLButtonElement
+const clear = document.getElementById('C') as HTMLButtonElement
+
+
 let btns:string = ''
+let armazenandoAcumulador:number = 0
 let result:number;
-let operado:string
 let numeros:number;
 let numero2:number;
-const regex = /[^\W]/gi
+
+let char:string = ''
 
 
 btn.forEach((element)=>{
+   try{
     element.addEventListener('click',(ev:Event)=>{
-       const {target} = ev 
-       if(target){
-        const dado1 = (target as HTMLButtonElement).textContent    
-        if(dado1){
-           btns += dado1;
-           numeros = parseFloat(btns) 
+        const {target} = ev 
+        if(target){
+         const dado1 = (target as HTMLButtonElement).textContent 
+         if(dado1){
+            btns += dado1;
+            numero2 = parseFloat(btns)
+            visor.value = btns.toString()
+ //O sinal recebido pelo btns altera o valor do resultado das expressões 
           
+         }
         }
-       }
-    visor.value = btns.toString()
+       
+     })
+   }catch(error){
+     throw new Error("Erro de conversão")
+   }
+
 })
+clear.addEventListener("click",()=>{
+    if(btns.includes('c') || btns.includes('C')){
+       btns = ''
+       numeros = 0;
+       numero2 = 0;
+       result = 0;
+       visor.value = '0'
+    } 
+  })  
+    
+soma.addEventListener('click',()=>{
+  
+   if(btns.includes("+")){
+     numeros = parseFloat(btns)
+     console.log(numeros,'vinda função +')
+     console.log(btns,'btns')
+     btns = ''
+     char = '+'
+   }else{
+    visor.value = ''
+   }
+ })
+
+ sub.addEventListener('click',()=>{
+    if(btns.includes("-")){
+        numeros = parseFloat(btns)
+        console.log(numeros,'vinda função -')
+        console.log(btns,'btns')
+        btns = ''
+        char = '-'
+      }
+ })
+
+
+
+
+
+
+
+
+results?.addEventListener('click',()=>{
+     if(btns.includes("=")){
    
 
-})
+        visor.value = ''
+        if(char.includes('+') && btns.includes('')){
+            result= numero2 + numeros 
+            armazenandoAcumulador = result
+           // if(char.includes('+') || btns != '') {
+          //      console.log(armazenandoAcumulador)
+           //     result = armazenandoAcumulador + numero2
+           // } 
+        }else if(char.includes('-') && btns.includes('')){
+            result = numeros - numero2
+        }
+     } 
+     
+   console.log(numeros,'numero1')
+   console.log(numero2,'numero2')
+   console.log(result,'resultado')
+   btns = ''
+   visor.value = result.toString()
+})   
 
- soma.addEventListener('click',()=>{
-    if(result){
-        numeros = result
-    }
-    numero2 = numeros
-    operado = btns
-    btns = '' 
- })
 
 
- sub.addEventListener('click', () =>{
-    if(result){
-        numeros = result
-    }
-    numero2 = numeros
-    operado = btns
-    btns = '' 
- })
- multiplicar.addEventListener('click',()=>{
-    if(result){
-        numeros = result
-    }
-    numero2 = numeros
-    operado = btns
-    btns = '' 
- })
- dividir.addEventListener('click',()=>{
-    if(result){
-        numeros = result
-    }
-    numero2 = numeros
-    operado = btns
-    btns = '' 
- })
-results?.addEventListener('click',()=>{
-    if(operado.includes('+')){
-        result = numeros + numero2
-    }else if(operado.includes('-')){
-        numeros > numero2? result = numeros - numero2: result = numero2 - numeros
-    }else if(operado.includes('x')){
-        result = numero2 * numeros
-    }else if(operado.includes('/')){
-        result = numero2 / numeros
-    }
-    
-    btns = ''
-    visor.value = result.toString()
+   
+
+ 
+
+
+
  
     
-   
-})
+ 
+
 
 
